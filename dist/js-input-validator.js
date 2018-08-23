0db512d89fc6c0e4edc65ae8f2d3f969a7842a41
thinkholic <mailtokasun@gmail.com>
2018-08-23 16:19:21 +0530

    v0.0.3

---
+++ dist/js-input-validator.js
@@ -0,0 +1,249 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define("js-input-validator", [], factory);
+	else if(typeof exports === 'object')
+		exports["js-input-validator"] = factory();
+	else
+		root["js-input-validator"] = factory();
+})(this, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports) {
+
+	"use strict";
+	
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
+	var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+	
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	// jsPrototype
+	Array.prototype.drop = function (deleteValue) {
+	  for (var i = 0; i < this.length; i++) {
+	    if (this[i] == deleteValue) {
+	      this.splice(i, 1);
+	      i--;
+	    }
+	  }
+	  return this;
+	};
+	
+	Object.prototype.isEmpty = function () {
+	  var obj = this;
+	  return Object.keys(obj).length === 0;
+	};
+	
+	// Utils.
+	function sanitize() {
+	  var value = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : null;
+	
+	  if (!value) return null;
+	  if (typeof value !== "string") return value;
+	
+	  return value.trim();
+	}
+	
+	function validateEmail(email) {
+	  var patt = new RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+	  if (!patt.test(email)) return "Invalid email address";
+	
+	  return null;
+	}
+	
+	function validateLatitude(latitude) {
+	  if (!(isFinite(latitude) && Math.abs(latitude) <= 90)) return "Invalid latitude!";
+	
+	  return null;
+	}
+	
+	function validateLongitude(longitude) {
+	  if (!(isFinite(longitude) && Math.abs(longitude) <= 180)) return "Invalid longitude!";
+	
+	  return null;
+	}
+	
+	function validateUrl(url) {
+	  var patt = new RegExp(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+	  if (!patt.test(url)) return "Invalid URL!";
+	
+	  return null;
+	}
+	
+	function validateArray(arr) {
+	  if (!Array.isArray(arr)) return "Invalid data type!";
+	
+	  return null;
+	}
+	
+	function validateType(value, type) {
+	  if (!value || !type) return null;
+	
+	  if (type === "email") return validateEmail(value);
+	  if (type === "latitude") return validateLatitude(value);
+	  if (type === "longitude") return validateLongitude(value);
+	  if (type === "url") return validateUrl(value);
+	  if (type === "array") return validateArray(value);
+	
+	  if ((typeof value === "undefined" ? "undefined" : _typeof(value)) !== type) return "Invalid data type!";
+	
+	  return null;
+	}
+	
+	function validateTypes(value) {
+	  var types = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
+	
+	  if (!value) return null;
+	  if (!types.includes(value)) return "Invalid data type!";
+	
+	  return null;
+	}
+	
+	function validateLength(value, length) {
+	  var charLength = String(value).length;
+	
+	  if (typeof length === "number" && charLength !== length) return "Invalid character size!";
+	
+	  if ((typeof length === "undefined" ? "undefined" : _typeof(length)) === "object") {
+	    if (length.min && charLength < length.min) return "Invalid character size! (min)";
+	    if (length.max && charLength < length.max) return "Invalid character size! (max)";
+	  }
+	
+	  return null;
+	}
+	
+	function validateMinMax(value, min, max) {
+	  if (typeof value !== "number") return null;
+	  if (min && !max && value < min) return "Should be greater than " + min + "!";
+	  if (!min && max && value > max) return "Should be less than " + max + "!";
+	  if (min && max && (value > max || value < min)) return "Should be between " + min + " and " + max + "!";
+	
+	  return null;
+	}
+	
+	function validateWithCustomMethod(value, values, validate) {
+	  if (!validate || typeof validate !== "function") return null;
+	
+	  if (!validate(value, values)) return "Invalid";
+	
+	  return null;
+	}
+	
+	// Validator
+	
+	var Validator = function () {
+	  function Validator(schema) {
+	    _classCallCheck(this, Validator);
+	
+	    this.schema = schema;
+	    this.errors = null;
+	  }
+	
+	  _createClass(Validator, [{
+	    key: "validate",
+	    value: function validate() {
+	      var _this = this;
+	
+	      var values = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+	
+	      var schema = this.schema;
+	      this.errors = {};
+	
+	      var fields = Object.keys(schema);
+	      fields.forEach(function (field) {
+	        var errorMessages = [];
+	
+	        var attr = schema[field];
+	        var value = sanitize(values[field]);
+	
+	        if (value && attr.type && typeof attr.type === "string") errorMessages.push(validateType(value, attr.type));
+	        if (value && attr.type && _typeof(attr.type) === "object" && Array.isArray(attr.type)) errorMessages.push(validateTypes(value, attr.type));
+	        if (value && attr.length) errorMessages.push(validateLength(value, attr.length));
+	        if (value && (attr.min || attr.max)) errorMessages.push(validateMinMax(value, attr.min, attr.max));
+	        if (value && attr.validate) errorMessages.push(validateWithCustomMethod(value, values, attr.validate));
+	
+	        if (attr.required && !value) errorMessages.push(attr.name + " is required!");
+	
+	        errorMessages.drop(null);
+	        if (errorMessages && errorMessages.length > 0) {
+	          _this.errors[field] = errorMessages;
+	        }
+	      });
+	
+	      return this.errors;
+	    }
+	  }, {
+	    key: "run",
+	    value: function run(values) {
+	      var list = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false;
+	
+	      var errors = this.validate(values);
+	      return list ? Object.values(errors) : errors;
+	    }
+	  }, {
+	    key: "isValid",
+	    value: function isValid(values) {
+	      var errors = this.validate(values);
+	      return errors.isEmpty();
+	    }
+	  }, {
+	    key: "getErrors",
+	    value: function getErrors() {
+	      return this.errors;
+	    }
+	  }]);
+	
+	  return Validator;
+	}();
+	
+	module.exports = Validator;
+
+/***/ }
+/******/ ])
+});
+;
+//# sourceMappingURL=js-input-validator.js.map
